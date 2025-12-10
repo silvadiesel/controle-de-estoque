68e7acefc179d3d896376b4d094750f9ca55ba10
Larissa Alves <alves04.larissa@gmail.com>
2025-12-10 15:59:07 -0300

    feat: implementar CRUD para peças no sistema de controle de estoque

---
+++ db/schema/pecas.ts
@@ -0,0 +1,18 @@
+import { categorias } from './categorias';
+import { fornecedor } from './fornecedor';
+import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+
+export const pecas = pgTable('pecas', {
+  id: serial('id').primaryKey(),
+  name_peca: text('name_peca').notNull(),
+  data_cadastro: timestamp('data_cadastro').defaultNow(),
+  codigo: text('codigo').notNull(),
+  categoria_id: integer('categoria_id').references(() => categorias.id),
+  quantidade: integer('quantidade').notNull(),
+  preco: integer('preco').notNull(),
+  fornecedor_id: integer('fornecedor_id').references(() => fornecedor.id),
+  localizacao: integer('localizacao').array()
+});
+
+export type Peca = typeof pecas.$inferSelect;
+export type NewPeca = typeof pecas.$inferInsert;
